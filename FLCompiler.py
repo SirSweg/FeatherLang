14cc6855c09d4b38529c2dfc5cb0e074147fea9d
SirSweg <127277179+SirSweg@users.noreply.github.com>
2024-05-09 22:09:17 -0400

    Restart FLC Code
    Started over with a better idea of what to do, made initial file open steps

--- FLCompiler.py
+++ FLCompiler.py
@@ -1,70 +1,16 @@
 from collections import deque
 import sys
 
-# list of all functions for matching purposes in compiler
-funcList = ["pr", "+", "-", "/", "*", "if", "ef", "df", "fn"]
-
-# selects the input file and makes a String containing each character
-file = open(sys.argv[1])
-fileStr = file.read()
-file.close()
-
-# adds each char in fileStr to compilerChars and handles special characters
+# To-Do
+# 1. Take input file and create a .py file that shares the same name
+# 2. Convert the input file FeatherLang code into a bytecode like string
+#    that will be put into the compiler
+# 3. Read bytecode string and print corresponding python code to the .py file
 # ΘΣΨ
-compilerChars = deque([])
-for x in fileStr:
-    match x:
-        case ' ':
-            compilerChars.append('Θ')
-        case '\t':
-            compilerChars.append('Θ')
-        case '\n':
-            compilerChars.append('Ψ')
-        case '(':
-            continue
-        case ')':
-            continue
-        case _:
-            compilerChars.append(x)
-# ~ denotes end of file
-compilerChars.append('Σ')
-        
-# interprets each char in compilerChars meaning in FeatherLang and converts
-# it into Python Code
-pyFile = open("fl.py", "w")
 
-def compile(matchStr):
-    x = compilerChars.popleft()
-    match x:
-        case 'Θ':
-            compile("")
-        case 'Ψ':
-            compile("")
-        case 'Σ':
-            exit()
-        case _:
-            matchStr += x
-            if matchStr in funcList:
-                func = globals()[matchStr]
-                func()
-                compile("")
-            else:
-                compile(matchStr)
+# this is the input file with the FeatherLang code
+flFile = open(sys.argv[1])
 
-# definitions of built in functions
-def pr():
-    # removing space between pr and function input
-    compilerChars.popleft()
-    tmp = ""
-
-    while compilerChars[0] != 'Ψ' and compilerChars[0] != 'Σ':
-        if compilerChars[0] == 'Θ':
-            compilerChars.popleft()
-            tmp += " "
-        else:
-            tmp += compilerChars.popleft()
-
-    pyFile.write("print(\'" + tmp + "\')")
-
-# finally run the compiler
-compile("")
+# gets the name of the file w/o .txt and makes a corresponding .py
+nameLength = len(flFile.name)
+pyFile = open(flFile.name[:nameLength-4] + ".py", "w")
