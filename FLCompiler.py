fc927ec60fe409554e6bf4b64dba1292b1c9ca90
SirSweg <127277179+SirSweg@users.noreply.github.com>
2024-05-12 13:55:14 -0400

    Make working compiler-ready queue converter
    The compiler-ready queue now successfully takes the input file text and turns it into segmented chunks in the compilerQueue

--- FLCompiler.py
+++ FLCompiler.py
@@ -14,3 +14,47 @@ flFile = open(sys.argv[1])
 # gets the name of the file w/o .txt and makes a corresponding .py
 nameLength = len(flFile.name)
 pyFile = open(flFile.name[:nameLength-4] + ".py", "w")
+
+# How to turn code into compiler-ready string
+# - ignore () turn them into spaces?
+# - turn spaces into \s
+# - if you find \( or \), replace with just ( or )
+# - \e at end of file if necessary
+# - ; ignore everything until end of line
+
+# Making compiler-ready queue:
+# 1. add chars from flFileStr to tmpCompilerStr until you meet a space or an 
+#    equivalent
+# 2. add tmpCompilerStr to compilerQueue, then set tmpCompilerStr to ""
+compilerQueue = deque([])
+flFileStr = flFile.read()
+tmpCompilerStr = ""
+
+for x in flFileStr:
+    match x:
+        # Spaces and their equivalents
+        case ' ':
+            compilerQueue.append(tmpCompilerStr)
+            tmpCompilerStr = ""
+        case '\t':
+            compilerQueue.append(tmpCompilerStr)
+            tmpCompilerStr = ""
+        case '(':
+            compilerQueue.append(tmpCompilerStr)
+            tmpCompilerStr = ""
+        case ')':
+            compilerQueue.append(tmpCompilerStr)
+            tmpCompilerStr = ""
+        # Comments
+        case ';':
+            compilerQueue.append(x)
+        # New Lines
+        case '\n':
+            compilerQueue.append(tmpCompilerStr)
+            tmpCompilerStr = ""
+            compilerQueue.append(x)
+        # Adding regular chars
+        case _:
+            tmpCompilerStr += x
+
+print(compilerQueue)
